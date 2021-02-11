650b124c99e46d185cdd67e87ddc63db9a9a7d0d
Luc-0 <luc0x00@gmail.com>
2021-02-11 16:39:26 -0300

    Add navbar component and some css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import './App.css';
+
 function App() {
   return (
-    <div>
-      <div></div>
+    <div className="app">
+      <Router>
+        <NavBar />
+      </Router>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <h1>Shopping Cart</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/shop">Shop</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;

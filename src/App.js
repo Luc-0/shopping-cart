153d87ec19be05c6b28f55915e858c3a9106dfc2
Luc-0 <luc0x00@gmail.com>
2021-02-11 18:06:41 -0300

    Add home component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
 import './App.css';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="app">
       <Router>
         <NavBar />
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
       </Router>
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="homepage">
+      <h2>Welcome to the Shopping Cart Project!</h2>
+    </div>
+  );
+};
+
+export default Home;
